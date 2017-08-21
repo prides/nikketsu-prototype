889515a44e4db29121f80a36af8648390fbb5c60
unknown <prides.ykt@gmail.com>
2017-08-21 23:00:23 +0900

    FightBot added

--- Assets/Scripts/SimplePlayerMovement.cs
+++ Assets/Scripts/SimplePlayerMovement.cs
@@ -7,8 +7,16 @@ public class SimplePlayerMovement : MonoBehaviour
     public float jumpSpeed = 8.0F;
     public float gravity = 20.0F;
     private Vector3 moveDirection = Vector3.zero;
+    [SerializeField]
+    private Animator animator;
+    private float currentSpeed;
 
-    void Update()
+    private void Start()
+    {
+
+    }
+
+    private void Update()
     {
         CharacterController controller = GetComponent<CharacterController>();
         if (controller.isGrounded)
@@ -16,11 +24,17 @@ public class SimplePlayerMovement : MonoBehaviour
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
             moveDirection = transform.TransformDirection(moveDirection);
             moveDirection *= speed;
+            currentSpeed = moveDirection.magnitude;
+            if (Input.GetButtonDown("Jump"))
+                animator.SetTrigger("Jump");
             if (Input.GetButton("Jump"))
                 moveDirection.y = jumpSpeed;
 
         }
+        if (Input.GetMouseButtonDown(0))
+            animator.SetTrigger("Punch");
         moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
+        animator.SetFloat("Speed", currentSpeed);
     }
 }
